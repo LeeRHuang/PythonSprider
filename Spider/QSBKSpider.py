5cd040ca36702fb359cb96e3530d8e5588541d86
lirihuang <lirihuang@ppdai.com>
2017-08-14 14:38:30 +0800

    修改糗百爬虫

--- Spider/QSBKSpider.py
+++ Spider/QSBKSpider.py
@@ -1,50 +1,67 @@
 # -*- coding: utf-8 -*-
-import urllib
 import os
+import re
 import urllib2
 from bs4 import BeautifulSoup
 
-url = 'https://www.qiushibaike.com/hot/'
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-Agent' : user_agent }
-req = urllib2.Request(url,headers=headers)
-response = urllib2.urlopen(req)
-html = response.read().decode('utf-8')
-soup = BeautifulSoup(html,'lxml')
+class QSBK:
 
-divs = soup.find_all(class_ = 'article block untagged mb15 typs_hot')
-for div in divs:
-    content = div.span.get_text()
-    print content
-
-foldPath = u'糗事百科'
-if not os.path.exists(foldPath):
-    os.mkdir(foldPath)
-path = foldPath + '/' + 'QSBK.txt'
-f = file(path, 'w')
-f.write(content.encode("utf8"))
-f.close()
+   def sprider(self):
+       i = 1 #定义一个索引变量
+       soup = getSoup() #初始化网页解析库实例
+       f = fileHandler() #初始化文件
+       getContent(soup,f,i) #解析获取内容
 
 
-#
-#
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-#
-# base_url = 'https://www.qiushibaike.com/hot/'  # 设定一个网址不变的部分，然后我们只要每次在这个后面加数字就可以了
-#
-# for num in range(1, 5): # 设置循环，让num分别等于1-10
-#     print('第{}页'.format(num))
-#
-#     r = requests.get(base_url + str(num), headers = headers) #这里对网址进行一个修改
-#
-#     #剩下的部分都是和原来的代码一样
-#     content = r.text
-#     soup = BeautifulSoup(r.text)
-#
-#     divs = soup.find_all(class_ = 'article block untagged mb15 typs_hot')
-#
-#     for div in divs:
-#         if div.find_all(class_ = 'thumb'):
-#             continue
-#         joke = div.span.get_text()
-#         print(joke)
+def loadData():
+    url = 'https://www.qiushibaike.com/' #其实百科首页地址
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' #UA
+    headers = {'User-Agent': user_agent}
+    req = urllib2.Request(url, headers=headers) #头部添加UA
+    response = urllib2.urlopen(req) #发起请求
+    return response
+
+
+def getSoup():
+    response = loadData()
+    html = response.read().decode('utf-8') #获取网页内容
+    soup = BeautifulSoup(html, 'lxml') #通过美丽鸡汤以xml形势解析
+    return soup
+
+
+def fileHandler():
+    foldPath = u'糗事百科'
+    if not os.path.exists(foldPath):
+        os.mkdir(foldPath)
+    path = foldPath + '/' + 'QSBK.txt'
+    f = file(path, 'w')
+    return f
+
+
+def writeData(c,f,i):
+    f.write('第%s条' % i)
+    f.write(c.encode('utf8'))
+    f.write('\n')
+
+
+def getContent(soup,f,i):
+    article = soup.find_all(class_=re.compile("article block untagged")) #通过article block untagged标签解析tag节点
+    for item in article:
+        username = item.h2.get_text() #获取用户昵称
+        img = item.img['src'] #获取头像
+        contents = item.select('.content') #获取内容tag节点
+        content = contents[0].span.get_text() #
+        number = item.select('.number') #获取number列表
+        vote = number[0].get_text() #点赞数
+        comment = number[1].get_text() #评论数
+
+        c = u"昵称:%s内容:%s点赞数:%s\n评论数:%s '\n' " %(username,content,vote,comment)
+        writeData(c, f, i) #写入文件
+        i += 1
+    f.close()
+
+
+#主程序入口
+if __name__ == '__main__':
+    qsbk = QSBK()
+    qsbk.sprider()
--- Spider/ZhihuSpider.py
+++ Spider/ZhihuSpider.py
@@ -1,54 +1,131 @@
 # -*- coding: utf-8 -*-
+
+__author__ = 'loveQt'
+
+import json
+import os
+import time
+import re
 import requests
 import ConfigParser
+import sys
+import xlwt
+from bs4 import BeautifulSoup
 
-def create_session():
+Zhihu = 'http://www.zhihu.com/'
+Login_url = Zhihu + 'login'
+
+
+# Vote_url = Zhihu + 'answer/' + ans_id +'/voters_profile?total=99999&offset='+str(num)+'0'
+def login():
     cf = ConfigParser.ConfigParser()
-    cf.read('config.ini')
-    cookies = cf.items('cookies')
+    cf.read("config.ini")
+    cookies = cf._sections['cookies']
+
+    email = cf.get("info", "email")
+    password = cf.get("info", "password")
     cookies = dict(cookies)
-    from pprint import pprint
-    pprint(cookies)
-    email = cf.get('info', 'email')
-    password = cf.get('info', 'password')
-
-    session = requests.session()
-    login_data = {'email': email, 'password': password}
+    global s
+    s = requests.session()
+    login_data = {"email": email, "password": password}
     header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
-        'Host': 'www.zhihu.com',
-        'Referer': 'http://www.zhihu.com/'
+        'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0",
+        'Host': "www.zhihu.com",
+        'Referer': "http://www.zhihu.com/",
+        'X-Requested-With': "XMLHttpRequest"
     }
-    r = session.post('http://www.zhihu.com/login/email', data=login_data, headers=header)
-    if r.json()['r'] == 1:
-        print 'Login Failed, reason is:',
-        for m in r.json()['data']:
-            print r.json()['data'][m]
-        print 'So we use cookies to login in...'
-        has_cookies = False
-        for key in cookies:
-            if key != '__name__' and cookies[key] != '':
-                has_cookies = True
-                break
-        if has_cookies is False:
-            raise ValueError('请填写config.ini文件中的cookies项.')
-        else:
-            # r = requests.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
-            r = session.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
-
-    with open('login.html', 'w') as fp:
-        fp.write(r.content)
-
-    return session, cookies
+    r = s.post(Login_url, data=login_data, headers=header)
 
 
-if __name__ == '__main__':
-    requests_session, requests_cookies = create_session()
 
-    # url = 'http://www.zhihu.com/login/email'
-    url = 'http://www.zhihu.com/topic/19552832'
-    # content = requests_session.get(url).content # 未登陆
-    # content = requests.get(url, cookies=requests_cookies).content # 已登陆
-    content = requests_session.get(url, cookies=requests_cookies).content # 已登陆
-    with open('url.html', 'w') as fp:
-        fp.write(content)
+
+def get_voters():
+    # 两种思路
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    sheet = book.add_sheet('data', cell_overwrite_ok=True)
+    login()
+    ans_id = raw_input('请输入抓包获得的问题id：')
+    Vote_url = Zhihu + 'answer/' + ans_id + '/voters_profile'
+    h = s.get(Vote_url)
+    html = h.content.encode('utf-8')
+    # print html
+    target = json.loads(html)
+    print '总赞同数：',
+    print target['paging']['total']
+    # total = target['paging']['total']
+    # Vote_url = 'http://www.zhihu.com/'+target['paging']['next']
+    num = 0
+    while target['paging']['next']:
+        try:
+            h = s.get(Vote_url)
+        except:
+            time.sleep(2)
+            h = s.get(Vote_url)
+        html = h.content.encode('utf-8')
+        # print html
+        target = json.loads(html)
+        Vote_url = 'http://www.zhihu.com' + target['paging']['next']
+
+        # print html
+        # 获取用户名
+        i = 10 * num
+        name = r'a title=\\"(.+?)\\"'
+        namelist = re.findall(name, html)
+        for each in namelist:
+            # print each.decode("unicode-escape")
+            sheet.write(i, 0, each.decode("unicode-escape"))
+            i = i + 1
+            # 获取用户地址
+        i = 10 * num
+        userurl = r'href=\\"(http://www.zhihu.com/people/.*?)\\'
+        userurllist = re.findall(userurl, html)
+        # print voteslist
+        for each in userurllist:
+            # print each
+            sheet.write(i, 5, each)
+            i = i + 1
+        # 获取点赞
+        i = 10 * num
+        votes = r'([_a-zA-Z0-9_]{0,10}) \\u8d5e\\u540c'
+        voteslist = re.findall(votes, html)
+        # print voteslist
+        for each in voteslist:
+            # print each
+            sheet.write(i, 1, each)
+            i = i + 1
+
+        # 获取感谢
+        i = 10 * num
+        thank = r'([_a-zA-Z0-9_]{0,10}) \\u611f\\u8c22'
+        thanklist = re.findall(thank, html)
+
+        for each in thanklist:
+            # print (each)
+            sheet.write(i, 2, each)
+            i = i + 1
+        # 获取提问
+        i = 10 * num
+        ques = r'([_a-zA-Z0-9_]{0,10}) \\u63d0\\u95ee'
+        queslist = re.findall(ques, html)
+        for each in queslist:
+            # print (each)
+            sheet.write(i, 3, each)
+            i = i + 1
+        # 获取回答
+        i = 10 * num
+        ans = r'([_a-zA-Z0-9_]{0,10}) \\u56de\\u7b54'
+        anslist = re.findall(ans, html)
+        for each in anslist:
+            # print (each)
+            sheet.write(i, 4, each)
+            i = i + 1
+        num = num + 1
+
+    book.save(r'.\\' + str(ans_id) + 'v2result.xls')
+    print 'Mission Complete'
+
+
+start = time.clock()
+get_voters()
+end = time.clock()
+print end - start
